c0d2177561304bfd7a13450ba0b809835e52cf3f
mickova2 <mickova2@spsejecna.cz>
2024-03-11 11:05:14 +0100

    animals, flower

---
+++ src/Flower.java
@@ -0,0 +1,25 @@
+public abstract class Flower {
+
+        protected String name;
+        protected double price;
+        protected double neededArea;
+        protected double chanceOfGrowth;
+
+        public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
+            this.name = name;
+            this.price = price;
+            this.neededArea = neededArea;
+            this.chanceOfGrowth = chanceOfGrowth;
+        }
+
+        @Override
+        public String toString() {
+            return "\nFlower" +
+                    "\nname: " + name +
+                    "\nprice: " + price +
+                    "\nneededArea: " + neededArea +
+                    "\nchanceOfGrowth: " + chanceOfGrowth;
+        }
+
+
+}
